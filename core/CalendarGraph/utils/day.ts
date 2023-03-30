2bfc698b7e469c9f539e44aa3d9f7d51cac25a89
llx-hp <lilongxiang2000@gmail.com>
2023-03-30 15:40:33 +0800

    feat: utils init

---
+++ core/CalendarGraph/utils/day.ts
@@ -0,0 +1,29 @@
+import dayjs from 'dayjs'
+import dayOfYear from 'dayjs/plugin/dayOfYear'
+
+dayjs.extend(dayOfYear)
+
+
+/**
+ * @returns
+ * ```js
+ * dayjs.Dayjs.format(day) // "Jan 01, 2022"
+ * ```
+ */
+export function dF(day: dayjs.Dayjs) {
+  return day.format('MMM DD, YYYY')
+}
+
+/** Get the days of the month */
+export function getMonthDays(year: number, month: number) {
+  return +dayjs().year(year).month(month).endOf('M').format('DD')
+}
+
+/** Get the day of the week on the first day of the month */
+export function getMouthFirstDay(year: number, month: number) {
+  return +dayjs().year(year).month(month).startOf('M').format('d')
+}
+
+export function getDateByDays(year: number, days: number) {
+  return dayjs().year(year).dayOfYear(days)
+}
---
+++ core/CalendarGraph/utils/log.ts
@@ -0,0 +1,3 @@
+export function log(...arg: any) {
+  console.log(...arg)
+}
