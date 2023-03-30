af0765ed1be40fb48435cbc638a08206d3635798
llx-hp <lilongxiang2000@gmail.com>
2023-03-30 15:44:42 +0800

    refactor: dir rename

---
+++ core/calendar-graph/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {  } from './utils/day'
+
+
+type TypeProps = {
+  year: number
+  isDark?: boolean
+}
+
+
+export default (props: TypeProps) => {
+  return (
+    <h1>hello</h1>
+  )
+}
